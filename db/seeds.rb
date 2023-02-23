02f6073fd2c0597126560db6a5b3dee6d6195abc
Jackson <jacksonjsanders@tamu.edu>
2023-02-23 00:28:04 +0000

    finalize

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,18 +6,18 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-# Seed the RottenPotatoes DB with some movies.
+Seed the RottenPotatoes DB with some movies.
 more_movies = [
-  {:title => 'My Neighbor Totoro', :rating => 'G',
-    :release_date => '16-Apr-1988'},
-  {:title => 'Green Book', :rating => 'PG-13',
-    :release_date => '16-Nov-2018'},
-  {:title => 'Parasite', :rating => 'R',
-    :release_date => '30-May-2019'},
-  {:title => 'Nomadland', :rating => 'R',
-    :release_date => '19-Feb-2021'},
-  {:title => 'CODA', :rating => 'PG-13',
-    :release_date => '13-Aug-2021'}
+  {:title => 'Fantastic Mr. Fox', :rating => 'PG',
+    :release_date => '23-Nov-2009'},
+  {:title => 'Batman', :rating => 'PG-13',
+    :release_date => '23-June-1989'},
+  {:title => 'Speed Racer', :rating => 'PG-13',
+    :release_date => '09-May-2008'},
+  {:title => 'Air Bud', :rating => 'G',
+    :release_date => '21-Feb-1997'},
+  {:title => 'March of the Penguins', :rating => 'G',
+    :release_date => '17-June-2005'}
 ]
 
 more_movies.each do |movie|
